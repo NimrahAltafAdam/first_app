9361c85906cfd5ee68e6fe191f486e5f467cdb3a
NimrahAltafAdam <nimrahadam.123@gmail.com>
2021-12-07 09:46:39 -0800

    Icons&Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,9 @@ void main() => runApp(MaterialApp(
 }*/
 
 //ADDING IMAGE
-class Home extends StatelessWidget {
+//image.network
+//image.asset
+/*class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override //to overide the build function inherited from stateless widget
@@ -58,6 +60,125 @@ class Home extends StatelessWidget {
       body: Center(
         child: Image.network('https://images.unsplash.com/photo-1638855519814-ce793e5efcaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
         ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}*/
+//BUTTONS & ICONS
+/*class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override //to overide the build function inherited from stateless widget
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+          child: Icon(
+            Icons.airport_shuttle,
+            color: Colors.lightBlue,
+            size: 50.0,
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}*/
+
+//RAISED BUTTONS
+
+/*class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override //to overide the build function inherited from stateless widget
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              print('you clicked me');
+            },
+            child: Text('click me'),
+            color: Colors.lightBlue,
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}*/
+
+//RAISED BUTTON + ICON
+/*class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override //to overide the build function inherited from stateless widget
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+          child: RaisedButton.icon(
+            onPressed: () {},
+            icon: Icon(
+              Icons.mail
+            ),
+            label: Text('mail me'),
+            color: Colors.amber
+          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}*/
+
+//ICON BUTTON
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override //to overide the build function inherited from stateless widget
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: IconButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          icon : Icon(Icons.alternate_email),
+          color: Colors.amber
+        )
+      ),
       floatingActionButton: FloatingActionButton(
         child: Text('click'),
         onPressed: () {},
