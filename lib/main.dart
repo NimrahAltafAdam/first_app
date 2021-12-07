5fd24e8fc533728d0de029ddc8b82bd39da06807
NimrahAltafAdam <nimrahadam.123@gmail.com>
2021-12-07 08:07:52 -0800

    Image&Assets

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() => runApp(MaterialApp(
 
 
 //CUSTOM WIDGET-stless
-class Home extends StatelessWidget {
+/*class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override //to overide the build function inherited from stateless widget
@@ -34,6 +34,30 @@ class Home extends StatelessWidget {
 
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}*/
+
+//ADDING IMAGE
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override //to overide the build function inherited from stateless widget
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.network('https://images.unsplash.com/photo-1638855519814-ce793e5efcaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
+        ),
       floatingActionButton: FloatingActionButton(
         child: Text('click'),
         onPressed: () {},
